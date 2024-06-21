863bf219741b28895b5dca1cc094145f4f966e4a
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2024-06-21 14:32:34 +0200

    Revert " increase send buffer to handle delayed acks in low latency transmissions"
    This reverts commit 3f6b2884916226e4546c1735cea6d6f72e8e71ea.

--- src/lwip/opt.h
+++ src/lwip/opt.h
@@ -449,7 +449,7 @@
  * (requires the LWIP_TCP option)
  */
 #if !defined MEMP_NUM_TCP_SEG || defined __DOXYGEN__
-#define MEMP_NUM_TCP_SEG                10 * 16
+#define MEMP_NUM_TCP_SEG                16
 #endif
 
 /**
@@ -1319,7 +1319,7 @@
  * as much as (2 * TCP_SND_BUF/TCP_MSS) for things to work.
  */
 #if !defined TCP_SND_QUEUELEN || defined __DOXYGEN__
-#define TCP_SND_QUEUELEN                ((10 * 4 * (TCP_SND_BUF) + (TCP_MSS - 1))/(TCP_MSS))
+#define TCP_SND_QUEUELEN                ((4 * (TCP_SND_BUF) + (TCP_MSS - 1))/(TCP_MSS))
 #endif
 
 /**
